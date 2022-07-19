c087036ae72fe866016c54a16c345ca96f5ed885
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:08:12 +0300

    Лекция 3. Задача 4. Сортировка выбором (массивы)

---
+++ Task_004-L3_4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Сортировка выбором
+int[] array = { 1, 5, 7, 9, 1, 8, 7, 6 };
+
+// метод печати массива
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array); // вызвали метод. напечатали массив
+
+Console.WriteLine();
+
+// метод сортировки
+void Sort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPos = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPos]) minPos = j;
+        }
+        int temporary = arr[i];
+        arr[i] = arr[minPos];
+        arr[minPos] = temporary;
+    }
+}
+Sort(array);
+PrintArray(array);
