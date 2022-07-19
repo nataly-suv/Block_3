7ab142cd1c4180d86f961eb3f6ee4151337a0ede
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:13:41 +0300

    Семинар 3. Задача 1. Поиск четверти оси координат

---
+++ Task_005-Sem3_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+// в которой находится эта точка.
+
+// Метод считывания координаты и возвращает массив с ней (точка x=34; y=-30)
+int[,] ReadPoint()
+{
+    string inputLine = Console.ReadLine();
+
+    string coordXLine = inputLine.Substring(0, inputLine.IndexOf(";")); // взяли подстроку от 0 до ; - x=34
+    coordXLine = coordXLine.Substring(coordXLine.IndexOf("=") + 1); //взяли подстроку от = до окнца - 34
+
+    string coordYLine = inputLine.Substring(inputLine.IndexOf(";") + 1);
+    coordYLine = coordYLine.Substring(coordYLine.IndexOf("=") + 1);
+
+    int coordX = int.Parse(coordXLine);
+    int coordY = int.Parse(coordYLine);
+
+    int[,] arrayOut = new int[1, 2]; // создали двумерный массив arrayOut
+    arrayOut[0, 0] = coordX; // положили в массив координату Х
+    arrayOut[0, 1] = coordY; // положили в массив координату У
+
+    return arrayOut;
+}
+
+// метод вывода номера четверти 
+void Answer(int[,] arrayPoint)
+{
+    if (arrayPoint[0, 0] > 0 && arrayPoint[0, 1] > 0)  Console.WriteLine("I четверть");
+    if (arrayPoint[0, 0] < 0 && arrayPoint[0, 1] > 0)  Console.WriteLine("II четверть");
+    if (arrayPoint[0, 0] < 0 && arrayPoint[0, 1] < 0)  Console.WriteLine("III четверть");
+    if (arrayPoint[0, 0] > 0 && arrayPoint[0, 1] < 0)  Console.WriteLine("IV четверть");
+}
+
+int[,] arrayPoint = ReadPoint();
+Answer(arrayPoint);
+
+
