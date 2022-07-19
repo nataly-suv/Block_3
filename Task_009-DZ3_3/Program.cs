f59d1addbff691f46c9574a2622078b82f368f3c
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:31:09 +0300

    ДЗ 3. Задача 3. Таблица кубов

---
+++ Task_009-DZ3_3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Выдает таблицу кубов от 1 до N 
+
+
+// метод вывода линии
+string Liniya(int x, string tire)
+{
+    string line = String.Empty;
+    for (int i = 0; i < x; i++)
+    {
+        line = line + tire;
+    }
+    //Console.WriteLine(line);
+    return line;
+}
+
+Console.WriteLine("Введите число");
+int inNum = int.Parse(Console.ReadLine());
+
+string num = "|";
+string num3 = "|";
+
+for (int i = 1; i <= inNum; i++)
+{
+    num = num + $" {i} |";
+    num3 = num3 + $" {Math.Pow(i, 3)} |";
+    if (num3.Length > num.Length)
+    {
+        string inst = new String(' ', (num3.Length-num.Length));
+        num = num.Insert((num.Length-1), inst);
+    }
+}
+
+Console.WriteLine(Liniya(num3.Length, "—"));
+Console.WriteLine(num);
+Console.WriteLine(Liniya(num3.Length, "—"));
+Console.WriteLine(num3);
+Console.WriteLine(Liniya(num3.Length, "—"));
+
+
+
