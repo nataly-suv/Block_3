db0846ebf0f17dee0ecd2eda3e78b4538c5c2c39
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:05:08 +0300

    Лекция 3. Задача 2. Работа со строками

---
+++ Task_003-L3_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пробела заменить |, букву к на К
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// есть строка string s = "abcd"
+//             индексы     0123
+// к элементу строки можно обратиться s[2]  - это буква с
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine (newText);
+Console.WriteLine ();
+
+string newText2 = Replace (newText, 'к', 'К');
+Console.WriteLine (newText2);
