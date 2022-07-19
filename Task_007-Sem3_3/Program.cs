d85b31bce3f3aceee587351f21ba793213c2e03d
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:18:32 +0300

    Семинар 3. Задача 3. Поиск расстояния между точками

---
+++ Task_007-Sem3_3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Програ принимает 2 точки и находит расстояние между ними
+
+// глобальные переменные
+int x1;
+int y1;
+int x2;
+int y2;
+double s;
+
+// метод. считывает координаты точек
+void ReadData()
+{
+    Console.WriteLine("Введите X1");
+    x1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите Y1");
+    y1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите X2");
+    x2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите Y2");
+    y2 = int.Parse(Console.ReadLine());
+}
+
+// метод. Вычмсление расстояние между х и у
+void Answer()
+{
+    s = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+}
+
+
+ReadData();
+Answer();
+Console.WriteLine(s);
