068ff3117f2c7dd3f98499decf674b3d12adb620
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-19 22:15:42 +0300

    Семинар 3. Задача 2. Диапозон координат по четверти

---
+++ Task_006-Sem3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Прграмма, по номеру четверти выдает диапозон координат 
+void answer(int x)
+{
+    if (x == 1) Console.WriteLine("x > 0, y > 0");
+    if (x == 2) Console.WriteLine("x < 0, y > 0");
+    if (x == 3) Console.WriteLine("x < 0, y < 0");
+    if (x == 4) Console.WriteLine("x > 0, y < 0");
+}
+
+Console.WriteLine("Введите номер четверти");
+string? inNum = Console.ReadLine();
+if (inNum != null)
+{
+    int num = int.Parse(inNum);
+    answer(num);
+}
