e5cd27f296880c292f687564eea1c05cc947e596
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-30 14:28:19 +0300

    temp

--- Task_010-Vramennaya/Program.cs
+++ Task_010-Vramennaya/Program.cs
@@ -1,19 +1,32 @@
 ﻿// Выдает таблицу квадратов от 1 до N
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine()); 
-// или int num = int.Parse(Console.ReadLine());
 
-string lineN = String.Empty;
-string lineN2 = String.Empty; 
-
-for (int count = 1; count <= num; count++)
+List<int> listGen(int number)
 {
-    lineN = lineN + $"{count} ";
-    lineN2 = lineN2 + $"{count * count} ";
+    List<int> numbers = new List<int>();
+    for (int i = 1; i <= number; i++)
+    {
+        numbers.Add(i);
+    }
+    return numbers;
 }
 
-Console.WriteLine(lineN);
-Console.WriteLine(lineN2);
+Console.WriteLine("Введите число");
+string? inputLine = Console.ReadLine();
 
- 
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    List<int> nums = listGen(inputNumber);
+    foreach (var i in nums)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+    foreach (var i in nums)
+    {
+        Console.Write(Math.Pow(i, 2));
+        Console.Write(" ");
+    }
+}
