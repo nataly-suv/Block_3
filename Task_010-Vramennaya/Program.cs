64a2494062d35f4209dff288f3f9886e8cebf59f
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-07-30 13:35:32 +0300

    temp

---
+++ Task_010-Vramennaya/Program.cs
@@ -0,0 +1,9 @@
+﻿// Цикл в цикле. вывод таблицы умножения на экран
+for (int i=2; i<=10; i++)
+{
+    for (int j=1; j<=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
